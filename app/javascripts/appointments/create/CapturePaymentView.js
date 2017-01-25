0918e71380c0d3a962ad1a43b936322b875d8e14
Roberto Alvarez <robertoatc02@gmail.com>
2017-01-24 19:48:19 -0600

    Fix form fiasco

--- app/javascripts/appointments/create/CapturePaymentView.js
+++ app/javascripts/appointments/create/CapturePaymentView.js
@@ -1,8 +1,20 @@
 import BaseView from 'javascripts/shared/BaseView';
 import CapturePaymentViewTpl from 'templates/appointments/create/CapturePaymentView';
 
-export default class extends BaseView() {
+export default class extends BaseView({
+    events: {
+        'submit #capture-payment-form': '_onPaymentFormSubmit'
+    }
+}) {
     initialize() {
         this.render(CapturePaymentViewTpl);
+
+        _.bindAll(this,
+            '_onPaymentFormSubmit');
+    }
+
+    _onPaymentFormSubmit(evt) {
+        evt.preventDefault();
+        console.log('payment form');
     }
 }
