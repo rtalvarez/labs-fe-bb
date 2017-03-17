a2a5fd8a449f5885352715153c19115d9429a8bd
Roberto Alvarez <robertoatc02@gmail.com>
2017-03-16 19:03:01 -0500

    Add safehtml filter to bannerview

--- app/javascripts/profile/ProfileView.js
+++ app/javascripts/profile/ProfileView.js
@@ -2,13 +2,19 @@ import BaseView from 'javascripts/shared/BaseView';
 import ProfileViewTpl from 'templates/profile/ProfileView';
 
 import ProfileDetailsView from 'javascripts/profile/ProfileDetailsView';
+import BannerView from 'javascripts/shared/BannerView';
 
 export default class extends BaseView() {
     initialize(config) {
         super.initialize(config);
 
         this._selectors = {
-            'profileDetails': '.profile-details-view'
+            profileDetails: '.profile-details-view',
+            missingDoBBanner: '.missing-dob-banner',
+        };
+
+        this._copy = {
+            missingDoBBanner: 'Te recomendamos <strong>proveer tu fecha de nacimiento</strong> para agilizar la creacion de citas',
         };
 
         this.preRender();
@@ -28,6 +34,12 @@ export default class extends BaseView() {
     initViews() {
         this.views.details = new ProfileDetailsView({
             el: this.$find('profileDetails')
+        });
+
+        this.views.missingDoBBanner = new BannerView({
+            type: 'warning',
+            msg: this._copy.missingDoBBanner,
+            el: this.$find('missingDoBBanner'),
         });
     }
 
